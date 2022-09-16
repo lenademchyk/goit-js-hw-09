04e518f1b27e15fe8d21aefa414fae9f4fa67a8f
Olena Demchyk <lenademchyk@gmail.com>
2022-09-17 00:40:46 +0200

    finished tasks

---
+++ src/js/02-timer.js
@@ -0,0 +1,142 @@
+// Завдання 2 - таймер зворотного відліку
+// Виконуй це завдання у файлах 02 - timer.html і 02 - timer.js.Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.Такий таймер може використовуватися у блогах та інтернет - магазинах, сторінках реєстрації подій, під час технічного обслуговування тощо.Подивися демо - відео роботи таймера.
+// Елементи інтефрейсу
+// HTML містить готову розмітку таймера, поля вибору кінцевої дати і кнопку, по кліку на яку, таймер повинен запускатися. Додай мінімальне оформлення елементів інтерфейсу.
+
+// Бібліотека flatpickr
+// Використовуй бібліотеку flatpickr для того, щоб дозволити користувачеві кросбраузерно вибрати кінцеву дату і час в одному елементі інтерфейсу. Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
+
+// // Описаний в документації
+// import flatpickr from "flatpickr";
+// // Додатковий імпорт стилів
+// import "flatpickr/dist/flatpickr.min.css";
+
+// Бібліотека очікує, що її ініціалізують на елементі input[type="text"], тому ми додали до HTML документу поле input#datetime-picker.
+
+// <input type="text" id="datetime-picker" />
+
+// Другим аргументом функції flatpickr(selector, options) можна передати необов'язковий об'єкт параметрів. Ми підготували для тебе об'єкт, який потрібен для виконання завдання. Розберися, за що відповідає кожна властивість в документації «Options», і використовуй його у своєму коді.
+// Вибір дати
+// Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr. Саме у ньому варто обробляти дату, обрану користувачем. Параметр selectedDates - це масив обраних дат, тому ми беремо перший елемент.
+
+// Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
+// Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
+// Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
+// Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+// Відлік часу
+// Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
+
+// Кількість днів може складатися з більше, ніж двох цифр.
+// Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00: 00: 00: 00.
+// НЕ БУДЕМО УСКЛАДНЮВАТИ
+// Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
+
+// Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
+
+// Форматування часу
+// Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто, якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію addLeadingZero(value), яка використовує метод padStart() і перед рендерингом інтефрейсу форматує значення.
+
+// Бібліотека повідомлень
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Для відображення повідомлень користувачеві, замість window.alert(), використовуй бібліотеку notiflix.
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timer.targetTime = selectedDates[0].getTime();
+    if (timer.targetTime - Date.now() <= 0) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled');
+    }
+  },
+};
+
+flatpickr(input, options);
+
+startBtn.addEventListener('click', () => {
+  timer.start();
+});
+stopBtn.addEventListener('click', () => {
+  timer.stop();
+});
+stopBtn.setAttribute('disabled', 'disabled');
+startBtn.setAttribute('disabled', 'disabled');
+
+const timer = {
+  intervalId: null,
+  targetTime: null,
+  start() {
+    this.intervalId = setInterval(() => {
+      if (this.targetTime - Date.now() <= 0) {
+        this.reset();
+      }
+      const currentTime = Date.now();
+      const deltaTime = this.targetTime - currentTime;
+      const time = convertMs(deltaTime);
+
+      updateClockFace(time);
+    }, 1000);
+
+    startBtn.setAttribute('disabled', 'disabled');
+    stopBtn.removeAttribute('disabled');
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    const time = convertMs(0);
+    updateClockFace(time);
+    timer.targetTime = Date.now();
+
+    stopBtn.setAttribute('disabled', 'disabled');
+    startBtn.removeAttribute('disabled');
+  },
+};
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minutesRef.textContent = `${minutes}`;
+  secondsRef.textContent = `${seconds}`;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,70 @@
+// Завдання 3 - генератор промісів
+// Виконуй це завдання у файлах 03-promises.html і 03-promises.js. Подивися демо-відео роботи генератора промісів.
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+// Бібліотека повідомлень
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
+
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  let delayPromise = Number(delayEl.value);
+
+  for (let i = 1; i <= amountEl.value; i += 1) {
+    createPromise(i, delayPromise).then(onSuccess).catch(onError);
+    delayPromise += Number(stepEl.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+
+function onSuccess(result) {
+  Notiflix.Notify.success(result);
+}
+
+function onError(error) {
+  Notiflix.Notify.failure(error);
 }
